9be6b2c3916dabbeb538a05ad966cef1351b5303
suryamohan567@gmail.com <suryamohan567@gmail.com>
2019-02-24 13:39:25 +0530

    LengthOfLongestSubsequence

---
+++ src/site/ibit/dp/LengthOfLongestSubSequence.java
@@ -0,0 +1,57 @@
+package site.ibit.dp;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LengthOfLongestSubSequence {
+
+    public static int longestSubsequenceLength(final List<Integer> A){
+        List<Integer> lis1 = new ArrayList<>();
+        List<Integer> lis2 = new ArrayList<>();
+
+        if(A.size() == 0) return 0;
+
+        int max = 0;
+
+        for(int i=0; i<A.size(); i++){
+            lis1.add(i,1);
+            lis2.add(i,1);
+        }
+
+        for(int i= 1;i<A.size();i++){
+            for(int j =0;j <i;j++){
+                if(A.get(i) > A.get(j) && lis1.get(i) < lis1.get(j) + 1){
+                    lis1.set(i,lis1.get(j) + 1);
+                }
+            }
+        }
+
+        for(int i= A.size()-2;i>=0;i--){
+            for(int j = A.size() -1 ;j>i;j--){
+                if(A.get(i) >  A.get(j) && lis2.get(i) < lis2.get(j) + 1){
+                    lis2.set(i,lis2.get(j) + 1);
+                }
+            }
+        }
+
+        for(int i=0 ;i<A.size();i++){
+            //System.out.println(max);
+            if(max < (lis1.get(i) + lis2.get(i) - 1 )){
+                max = lis1.get(i) + lis2.get(i) - 1;
+            }
+        }
+
+        System.out.println(lis1);
+        System.out.println(lis2);
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1, 11, 2, 10, 4, 5, 2, 1));
+        System.out.println(longestSubsequenceLength(list));
+    }
+}
