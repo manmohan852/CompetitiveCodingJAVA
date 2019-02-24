6a7e1b17487e369c96dad0d3b39e7743b0f59075
suryamohan567@gmail.com <suryamohan567@gmail.com>
2019-02-24 14:02:26 +0530

    WaysToDecode

---
+++ src/site/ibit/dp/WaysToDecode.java
@@ -0,0 +1,26 @@
+package site.ibit.dp;
+
+public class WaysToDecode {
+
+    public static int numDecodings(String A){
+        if(A==null || A.length() == 0)
+            return 0;
+
+        int[] count = new int[A.length() + 1];
+        count[0] = 1;
+        count[1] = 1;
+        for(int j=2;j<=A.length();j++){
+            count[j] = 0;
+            if(A.charAt(j-1) > '0')
+                count[j] = count[j-1];
+            if(A.charAt(j-2) == '1' || A.charAt(j-2) == '2' && A.charAt(j-1) < '7'){
+                count[j] +=count[j-2];
+            }
+        }
+        return count[A.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numDecodings("12"));
+    }
+}
