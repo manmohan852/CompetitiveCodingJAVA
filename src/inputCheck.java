f8182aea87baa910662b301324fe8b655133bae9
ManmohanGupta <manmohan852@gmail.com>
2022-12-05 13:20:14 +0530

    solved Snake and ladder

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,7 +1,13 @@
 public class inputCheck {
     public static void main(String[] args) {
-        int n = 15 / 9;
-        System.out.println(n);
+        String name = "manmohan";
+//        for(int i = 0; i < name.length(); i++){
+//            System.out.print(name.charAt(i));
+//        }
+        for(char i : name.toCharArray()){
+            System.out.print(i);
+        }
+        System.out.println("\n");
 
 
     }
