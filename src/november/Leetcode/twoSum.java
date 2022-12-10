fdf7bdb5e82584d749082066060b8477b4bb402f
ManmohanGupta <manmohan852@gmail.com>
2022-12-10 18:01:45 +0530

    solved TwoSum

--- src/november/Leetcode/twoSum.java
+++ src/november/Leetcode/twoSum.java
@@ -7,12 +7,24 @@ public class twoSum {
     public static void main(String[] args) {
         int[] nums = {2, 4, 6, 3, 9};
         int target = 15;
-        int[] ans = twoSum(nums, target);
+        int[] ans = twoSum3(nums, target);
         for(int i = 0; i < ans.length;i++){
             System.out.println(ans[i]);
         }
     }
-    public static int[] twoSum(int[] nums, int target) {
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
@@ -22,6 +34,18 @@ public class twoSum {
             if (map.containsKey(complement) && map.get(complement) != i) {
                 return new int[] { i, map.get(complement) };
             }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
         }
         // In case there is no solution, we'll just return null
         return null;
