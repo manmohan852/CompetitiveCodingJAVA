0fcd26a10c3c73c391e70a102af5d458379956c9
ManmohanGupta <manmohan852@gmail.com>
2022-12-16 13:51:40 +0530

    solved rainWaterTrapping

---
+++ src/MySite/TwoPointers/trappingRainWater.java
@@ -0,0 +1,35 @@
+package MySite.TwoPointers;
+
+public class trappingRainWater {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int ans = trap(height);
+        System.out.println(ans);
+    }
+    public static int trap(int[] height) {
+        int n = height.length;
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            if(height[max] <  height[i]){
+                max = i;
+            }
+
+        }
+        int leftmax = 0;
+        int sum = 0;
+        for(int i = 0; i < max; i++){
+            if(height[leftmax] < height[i]){
+                leftmax = i;
+            }
+            sum += Math.min(height[leftmax] , height[max]) - height[i];
+        }
+        int rightmax = n - 1;
+        for(int i = n - 1; i > max; i--){
+            if(height[rightmax] < height[i]){
+                rightmax = i;
+            }
+            sum += Math.min(height[rightmax] , height[max]) - height[i];
+        }
+        return sum;
+    }
+}
