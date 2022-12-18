f7425240afc9075c026209a0b1a3bb502d27bdf7
ManmohanGupta <manmohan852@gmail.com>
2022-12-18 22:00:29 +0530

    made some changes

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,16 +1,29 @@
-import java.util.Stack;
+// Java Code to show use of
+// super keyword with constructor
 
-class inputCheck{
-    public static void main(String[] args) {
-        String s = "(){}[)]";
-        Stack<Character> stack = new Stack<Character>();
-        stack.push(s.charAt(0));
-        if(stack.pop() != 'a') {
-            System.out.print("dfsdf");
-        }
-        System.out.println(stack.pop());
-        return ;
-
+// superclass Person
+class Person {
+    Person()
+    {
+        System.out.println("Person class Constructor");
     }
+}
+
+// subclass Student extending the Person class
+class Student extends Person {
+    Student()
+    {
+        // invoke or call parent class constructor
+        //super();
+
+        System.out.println("Student class Constructor");
+    }
+}
+
+// Driver Program
+class Test {
+    public static void main(String[] args)
+    {
+        Student s = new Student();
     }
 }
