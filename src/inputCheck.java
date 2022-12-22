865a32a8b8986df47dc1b17a1ad28579f417ad82
ManmohanGupta <manmohan852@gmail.com>
2022-12-22 22:36:18 +0530

    made some changes

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,27 +1,25 @@
+class Temp
+{
+    private Temp(int data)
+    {
+        System.out.printf(" Constructor called ");
+    }
+    protected static Temp create(int data)
+    {
+        Temp obj = new Temp(data);
+        return obj;
+    }
+    public void myMethod()
+    {
+        System.out.printf(" Method called ");
+    }
+}
+
 public class Test
 {
-    public Test()
-    {
-        System.out.printf("1");
-        new Test(10);
-        System.out.printf("5");
-    }
-    public Test(int temp)
-    {
-        System.out.printf("2");
-        new Test(10, 20);
-        System.out.printf("4");
-    }
-    public Test(int data, int temp)
-    {
-        System.out.printf("3");
-
-    }
-
     public static void main(String[] args)
     {
-        Test obj = new Test();
-
+        Temp obj = Temp.create(20);
+        obj.myMethod();
     }
-
 }
