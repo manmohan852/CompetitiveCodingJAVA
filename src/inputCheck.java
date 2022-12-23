972cf5e6856b1a8f4af453fdbb54c05fca05eeb3
ManmohanGupta <manmohan852@gmail.com>
2022-12-23 22:00:13 +0530

    made some changes

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,13 +1,21 @@
-public class Calculator
+class Alpha
 {
-    int num = 100;
-    public void calc(int num) { this.num = num * 10; }
-    public void printNum()	 { System.out.println(num); }
+    public String type = "a ";
+    public Alpha() { System.out.print("alpha "); }
+}
+
+public class Beta extends Alpha
+{
+    public Beta() { System.out.print("beta "); }
+
+    void go()
+    {
+        type = "b ";
+        System.out.print(this.type + super.type);
+    }
 
     public static void main(String[] args)
     {
-        Calculator obj = new Calculator();
-        obj.calc(2);
-        obj.printNum();
+        new Beta().go();
     }
 }
