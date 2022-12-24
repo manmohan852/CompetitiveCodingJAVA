f514a8dedaa9f096a36e224fab3efbdad8e580a2
ManmohanGupta <manmohan852@gmail.com>
2022-12-24 20:01:56 +0530

    some changes made

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,12 +1,21 @@
-public class Test
+public class MyStuff
 {
+    String name;
+
+    MyStuff(String n) { name = n; }
+
     public static void main(String[] args)
     {
-        StringBuilder s1 = new StringBuilder("Java");
-        String s2 = "Love";
-        s1.append(s2);
-        s1.substring(4);
-        int foundAt = s1.indexOf(s2);
-        System.out.println(foundAt);
+        MyStuff m1 = new MyStuff("guitar");
+        MyStuff m2 = new MyStuff("tv");
+        System.out.println(m2.equals(m1));
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        MyStuff m = (MyStuff) obj;
+        if (m.name != null) { return true; }
+        return false;
     }
 }
