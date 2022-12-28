d9d024d2a27f9a2ec5fbcb8d32e1d2c93e027381
ManmohanGupta <manmohan852@gmail.com>
2022-12-28 22:04:08 +0530

    mintifi question2 solved

--- src/november/interviews/ezetap/mintifi/questionTwo.java
+++ src/november/interviews/ezetap/mintifi/questionTwo.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class questionTwo {
     public static void main(String[] args) {
-        String str = "Java is great Python is    also great";
+        String str = "Java is great Python is also also great";
         String[] s= str.split(" ");
         Set<String> hs = new HashSet<>();
         for(int i = 0; i < s.length; i++){
