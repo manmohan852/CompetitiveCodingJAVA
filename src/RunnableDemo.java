68515afb23bd7711842f1fed085547f40ffbceb6
ManmohanGupta <manmohan852@gmail.com>
2022-12-29 22:38:47 +0530

    group anagrams

---
+++ src/RunnableDemo.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+//https://www.geeksforgeeks.org/runnable-interface-in-java/
+public class RunnableDemo {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Main thread is- " +
+                Thread.currentThread().getName());
+        Thread t1 = new Thread(new RunnableDemo().new RunnableImpl());
+        t1.start();
+    }
+
+    private class RunnableImpl implements Runnable {
+
+        public void run()
+        {
+            System.out.println(Thread.currentThread().getName()
+                    + ", executing run() method!");
+            /**
+             * Checked exception can't be thrown, Runnable must
+             * handle checked exception itself.
+             */
+            try {
+                throw new FileNotFoundException();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("Must catch here!");
+                e.printStackTrace();
+            }
+
+            //int r = 1 / 0;
+            /*
+             * Below commented line is an example
+             * of thrown RuntimeException.
+             */
+             throw new NullPointerException();
+        }
+    }
+}
