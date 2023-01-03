7cea94274bfd823a9fcc9028e9ef01f75c567418
ManmohanGupta <manmohan852@gmail.com>
2023-01-03 20:00:52 +0530

    made some changes

--- src/inputCheck.java
+++ src/inputCheck.java
@@ -1,18 +1,43 @@
-class SolarSystem {
-}
-class Earth extends SolarSystem {
-}
-class Mars extends SolarSystem {
-}
-public class Moon extends Earth {
-    public static void main(String args[])
-    {
-        SolarSystem s = new SolarSystem();
-        Earth e = new Earth();
-        Mars m = new Mars();
+// A Simple Java program to demonstrate
+// Overriding and Access-Modifiers
 
-        System.out.println(s instanceof SolarSystem);
-        System.out.println(e instanceof Earth);
-        System.out.println(m instanceof SolarSystem);
+class Parent {
+    // private methods are not overridden
+    private void m1()
+    {
+        System.out.println("From parent m1()");
+    }
+
+    protected void m2()
+    {
+        System.out.println("From parent m2()");
+    }
+}
+
+class Child extends Parent {
+    // new m1() method
+    // unique to Child class
+    private void m1()
+    {
+        System.out.println("From child m1()");
+    }
+
+    // overriding method
+    // with more accessibility
+    @Override
+    public void m2()
+    {
+        System.out.println("From child m2()");
+    }
+}
+
+// Driver class
+class Main {
+    public static void main(String[] args)
+    {
+        Parent obj1 = new Parent();
+        obj1.m2();
+        Parent obj2 = new Child();
+        obj2.m2();
     }
 }
