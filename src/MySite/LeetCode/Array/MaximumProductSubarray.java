c54cfbd6d88447c9ee4b294a888bfac2f4c7a9a6
ManmohanGupta <manmohan852@gmail.com>
2023-01-23 18:13:04 +0530

    maximum product subarray

---
+++ src/MySite/LeetCode/Array/MaximumProductSubarray.java
@@ -0,0 +1,26 @@
+package MySite.LeetCode.Array;
+
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+
+    }
+    public int maxProduct(int[] nums) {
+
+        int max = nums[0], min = nums[0], ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int temp = max;  // store the max because before updating min your max will already be updated
+
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+
+            if (max > ans) {
+                ans = max;
+            }
+        }
+
+        return ans;
+
+    }
+}
