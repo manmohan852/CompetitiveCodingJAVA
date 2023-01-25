d222074bb55f6de84216e95befa306e5571e2b6d
ManmohanGupta <manmohan852@gmail.com>
2023-01-25 19:01:04 +0530

    search in a sorted array

---
+++ src/MySite/LeetCode/Array/SearchinRotatedSortedArray.java
@@ -0,0 +1,33 @@
+package MySite.LeetCode.Array;
+
+public class SearchinRotatedSortedArray {
+    public static void main(String[] args) {
+
+    }
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int L = 0;
+        int R = n - 1;
+        while(L <= R){
+            int mid = L + (R - L) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[L] <= nums[mid]){
+                if(target >= nums[L] && target < nums[mid]){
+                    R = mid - 1;
+                }
+                else{
+                    L = mid + 1;
+                }
+            }
+            else{
+                if(target <= nums[R] && target > nums[mid]){
+                    L = mid + 1;
+                }
+                else{
+                    R = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
