146d218cc0b7b9a94b0c6fc112ce9cc7b204fdd2
ManmohanGupta <manmohan852@gmail.com>
2023-02-01 18:10:19 +0530

    3sum smaller

---
+++ src/MySite/LeetCode/TwoPointers/threeSumSmaller.java
@@ -0,0 +1,26 @@
+package MySite.LeetCode.TwoPointers;
+
+import java.util.Arrays;
+
+public class threeSumSmaller {
+    public static void main(String[] args) {
+
+    }
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int counter = 0, n = nums.length;
+        for(int i = 0; i < n; i++){
+            int L = i + 1, R = n - 1;
+            while(L < R){
+                int curSum = nums[L] + nums[R] + nums[i];
+                if(curSum < target){
+                    counter += R - L;
+                    L++;
+                }else {
+                    R--;
+                }
+            }
+        }
+        return counter;
+    }
+}
