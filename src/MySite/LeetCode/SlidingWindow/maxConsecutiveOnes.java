03db539e78d51dc81ebd21a9574eb270b115ad3a
ManmohanGupta <manmohan852@gmail.com>
2023-02-02 17:56:29 +0530

    max consecutive ones

---
+++ src/MySite/LeetCode/SlidingWindow/maxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package MySite.LeetCode.SlidingWindow;
+
+public class maxConsecutiveOnes {
+    public static void main(String[] args) {
+
+    }
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxLen = 0;
+        int counter = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                counter = 0;
+            }else{
+                counter++;
+                maxLen = Math.max(maxLen, counter);
+            }
+        }
+        return maxLen;
+    }
+}
