9801bbcb8ed8004945072631b544e7fbb0c4804c
ManmohanGupta <manmohan852@gmail.com>
2023-02-04 14:49:33 +0530

    Find Smallest Letter Greater Than Target

---
+++ src/MySite/LeetCode/BinarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,23 @@
+package MySite.LeetCode.BinarySearch;
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class FindSmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+
+    }
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        if(letters[n - 1] <= target || target < letters[0]){
+            return letters[0];
+        }
+        int L = 0, R = n - 1;
+        while(L + 1 < R){
+            int mid = L + (R - L) / 2;
+            if(letters[mid] <= target){
+                L = mid;
+            }else{
+                R = mid;
+            }
+        }
+        return letters[R];
+    }
+}
