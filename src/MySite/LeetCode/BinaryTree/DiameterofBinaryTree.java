b67f442be4475b4fd0af8b2d7e48f5a4f7d39061
ManmohanGupta <manmohan852@gmail.com>
2023-02-14 23:41:48 +0530

    level order traversal

---
+++ src/MySite/LeetCode/BinaryTree/DiameterofBinaryTree.java
@@ -0,0 +1,23 @@
+package MySite.LeetCode.BinaryTree;
+// https://leetcode.com/problems/diameter-of-binary-tree/description/
+public class DiameterofBinaryTree {
+    public static void main(String[] args) {
+
+    }
+    int max  = Integer.MIN_VALUE;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    private int dfs(TreeNode node){
+        int left = 0, right = 0;
+        if(node.left != null){
+            left = dfs(node.left);
+        }
+        if(node.right != null){
+            right = dfs(node.right);
+        }
+        max = Math.max(max, right + left);
+        return Math.max(left, right) + 1;
+    }
+}
